f47d84bbc47b03f539242566439a817b166d7d16
poornaderedla <durgareddy.deredla@sasi.ac.in>
2025-04-15 20:35:17 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import RegistrationForm from './Reg-Form';
+
+function App() {
+  return (
+    <div className="App">
+      <RegistrationForm />
+    </div>
+  );
+}
+
+export default App;
